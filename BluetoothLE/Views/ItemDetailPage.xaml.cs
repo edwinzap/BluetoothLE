05e1f1a312ffaace808df308abf99545739678b6
Miguel <forgetmiguel@hotmail.com>
2022-04-29 13:01:33 +0200

    Add project files.

---
+++ BluetoothLE/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BluetoothLE.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BluetoothLE.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
